b1e00b2394f85491fe2b520423d045b0ea1621bc
natalia.zc@hotmail.com <natalia.zc@hotmail.com>
2022-12-15 02:25:08 +0000

    exercicios

--- dia-02/exercicios.sql
+++ dia-02/exercicios.sql
@@ -38,5 +38,71 @@ AND vlAlturaCm > 5
 -- DBTITLE 1,Foco (+exercícios)
 -- Lista de pedidos com mais de um item
 
+SELECT DISTINCT idPedido
+FROM silver_olist.item_pedido
+WHERE idPedidoItem > 1
+
+
+-- COMMAND ----------
+
+-- Lista de pedidos que o frete é mais caro que o item
+
+SELECT *
+  FROM silver_olist.item_pedido
+  WHERE vlFrete > vlPreco
+
+
+-- COMMAND ----------
+
+-- Lista de pedidos que ainda não foram enviados
+
+SELECT *
+  FROM silver_olist.pedido
+  WHERE dtEnvio IS null
+  
+
+
+-- COMMAND ----------
+
+-- Lista de pedidos que foram entregues com atraso
+
 SELECT *
 FROM silver_olist.pedido
+WHERE dtEntregue > dtEstimativaEntrega
+
+-- COMMAND ----------
+
+-- Lista de pedidos que foram entregues com 2 dias de antecedência
+
+SELECT *
+FROM silver_olist.pedido
+WHERE (datediff (dtEntregue, dtEstimativaEntrega) < 2) 
+
+-- COMMAND ----------
+
+-- Lista de pedidos feitos em dezembro de 2017 e entregues com atraso
+
+SELECT *
+FROM silver_olist.pedido
+WHERE (month (dtPedido)=12) AND (year(dtPedido)=2017) AND dtEstimativaEntrega < dtEntregue 
+
+
+-- COMMAND ----------
+
+-- Lista de pedidos com avaliação menor ou igual a 4
+
+SELECT *
+FROM silver_olist.avaliacao_pedido
+WHERE vlNota <= '4'
+
+
+
+-- COMMAND ----------
+
+-- Lista de pedidos com 2 ou mais parcelas menores que R$20
+
+SELECT *
+FROM silver_olist.pagamento_pedido
+WHERE nrPacelas >= '2' AND vlPagamento < '20.00' 
+
+
